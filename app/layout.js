e511c4e1825534f65fa47981f170a5e2f8c2774c
Abdul1237700 <abdulrouf0812@gmail.com>
2026-03-06 23:39:26 +0500

    update: google analytics

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
-
+import Script from 'next/script';
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
